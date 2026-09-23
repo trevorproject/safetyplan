894d47cf16080b7dd6b957afdc805c76cf54dc20
Igor Aviles <agni_moloc@hotmail.com>
2026-09-23 14:03:09 -0600

    fix merge and build issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AppShell } from './components/layout/AppShell';
 import { WelcomePage } from './pages/WelcomePage';
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource/manrope/400.css'
-import '@fontsource/manrope/500.css'
-import '@fontsource/manrope/600.css'
-import '@fontsource/caveat/400.css'
-import '@fontsource/caveat/500.css'
 import './index.css'
 import App from './App.tsx'
 
